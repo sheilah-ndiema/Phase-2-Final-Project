f4453886a1384d892be28f14f3e6db0e8dadbeb6
Ndiema-S <you@example.com>
2022-10-04 19:54:57 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
 function App() {
+    const [menuItems, setMenuItems] = useState(items);
+    const [categories, setCategories] = useState([]);
 
+
+    return (
+        <main>
+            <section className="menu section">
+                <div className="title">
+                    <h2>Jiko Cafe' menu</h2>
+                    <div className="underline"></div>
+                </div>
+                <Categories categories={categories} />
+                <Menu items={menuItems} />
+            </section>
+        </main>
+    );
 }
 
-
-
-
-
-
 export default App;
+
